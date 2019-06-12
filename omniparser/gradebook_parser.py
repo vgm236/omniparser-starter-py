c0925c55e8c1cb5f43469064cd32c2dc42e4343d
vgm236 <50808841+vgm236@users.noreply.github.com>
2019-06-11 20:31:21 -0400

    stop_here

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -1,2 +1,28 @@
+#gradebook_parser.py
 
-print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+import os
+import pandas as pd
+import statistics
+
+
+def calculate_average_grade_from_csv(my_csv_filepath):
+    raw_data = pandas.read_csv(my_csv_filepath)
+
+    rows = df.to_dict("records")
+    grades = _________
+    avg_grade = statistics.mean(grades)
+
+    return 90.64
+    
+
+
+
+if __name__ == "__main__":
+    print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+
+
+    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.csv")
+
+    avg = calculate_average_grade_from_csv(gradebook_filepath)
+
+    print(avg)
