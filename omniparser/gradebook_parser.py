29d9ea0debbc769955843502fc83bf190c41557c
vgm236 <50808841+vgm236@users.noreply.github.com>
2019-06-12 19:36:29 -0400

    end_of_class

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -3,35 +3,76 @@
 import os
 import pandas
 import statistics
+import json
 
+#
+# CSV VERSION
+#
 
 def calculate_average_grade_from_csv(my_csv_filepath):
-# Uses the panda package to process the csv (df = dataframe, a special data type from the panda package)
+    
+    # Uses the panda package to process the csv (df = dataframe, a special data type from the panda package)
     df = pandas.read_csv(my_csv_filepath)
-# For this specific type of function, "records" is something that allow you to interact with whatever is inside the file
-    rows = df.to_dict("records")
-# Create a list of the grades that were in the file
-    grades = [r["final_grade"] for r in rows]
-# Average out the list
-    avg_grade = statistics.mean(grades)
-    # or avg_grade = df["final_grade"].mean
+    avg_grade = df["final_grade"].mean
+    # instead of all functions from rows to avg_grade
 
-
-# Returns what you want for the defined function
+    # For this specific type of function, "records" is something that allow you to interact with whatever is inside the file
+    #rows = df.to_dict("records")
+    # Create a list of the grades that were in the file
+    #grades = [r["final_grade"] for r in rows]
+    # Average out the list
+    #avg_grade = statistics.mean(grades)
+    
+    # Returns what you want for the defined function
     return avg_grade
   
-# This is used when the program is invoke from the command line
+    # This is used when the program is invoke from the command line
 if __name__ == "__main__": 
     print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
 
-# Opens the file. The "__file__" is a reference to the worksheet. We can used cd here as well
-# Os.path.dirname gets the directory name as well - leading to the file you care about
-# You need the "data" and "gradebook" to find it correctly
+    # Opens the file. The "__file__" is a reference to the worksheet. We can used cd here as well
+    # Os.path.dirname gets the directory name as well - leading to the file you care about
+    # You need the "data" and "gradebook" to find it correctly
   
     gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.csv") 
 
     print(gradebook_filepath)
 
     avg = calculate_average_grade_from_csv(gradebook_filepath)
+
+    print(avg)
+
+
+#
+# JSON VERSION
+#
+
+def calculate_average_grade_from_json(my_json_filepath):
+    
+    #Function to open json file and read it
+    with open(my_json_filepath, "r") as f:
+        file_contents = f.read()
+    # Indentify the list
+    gradebook = json.loads(file_contents)
+    # Create a list of dictionaries of the students data
+    students = gradebook["students"]
+    # Create a list of grades from the student data
+    grades = [s["finalGrade"] for s in students]
+    # Do the average
+    avg_grade = statistics.mean(grades)
+    return avg_grade
+
+
+
+if __name__ == "__main__": 
+
+    print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+  
+    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2018.json") 
+
+    print(gradebook_filepath)
+    #print(os.path.isfile(gradebook_filepath)) # Must show "True"
+
+    avg = calculate_average_grade_from_json(gradebook_filepath)
 
     print(avg)
--- test/gradebook_parser_test.py
+++ test/gradebook_parser_test.py
@@ -17,9 +17,9 @@ def test_calculate_average_grade_from_csv():
 # JSON VERSION
 #
 
-#def test_calculate_average_grade_from_json():
-#    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.json")
-#    assert calculate_average_grade_from_json(gradebook_filepath) == 90.64
+def test_calculate_average_grade_from_json():
+    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.json")
+    assert calculate_average_grade_from_json(gradebook_filepath) == 90.64
 
-#    prev_gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2018.json")
-#    assert calculate_average_grade_from_json(prev_gradebook_filepath) == 83.64
+    prev_gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2018.json")
+    assert calculate_average_grade_from_json(prev_gradebook_filepath) == 83.64
